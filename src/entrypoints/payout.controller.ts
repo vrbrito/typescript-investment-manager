cd175e7aec953551b962197b7d321f1d350c73d9
Vitor Ramalho <vitor.ramalho@bct.ect.ufrn.br>
2023-07-23 22:16:49 -0300

    [FEATURE] Extend Transaction/Payout controllers to include create action

--- src/entrypoints/payout.controller.ts
+++ src/entrypoints/payout.controller.ts
@@ -1,16 +1,34 @@
-import { Controller, Get } from "@overnightjs/core";
+import { Controller, Get, Post } from "@overnightjs/core";
+import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
 import { type Request, type Response } from "express";
 import { type InMemoryPayoutRepository } from "../adapters/repository/payout.repository";
-import { listPayouts } from "../services/payout.services";
+import { PayoutInput } from "../domain/entities/payout.types";
+import { listPayouts, registerPayout } from "../services/payout.services";
 
 @Controller("payouts")
 export class PayoutController {
 	public constructor(private readonly payoutRepository: InMemoryPayoutRepository) {}
 
-	@Get("")
+	@Get()
 	public async payouts(_: Request, res: Response): Promise<void> {
 		const payouts = await listPayouts(this.payoutRepository);
 
 		res.status(200).send(payouts);
+	}
+
+	@Post()
+	public async addPayout(req: Request, res: Response): Promise<void> {
+		const payoutInput: PayoutInput = plainToClass(PayoutInput, req.body);
+
+		await validate(payoutInput, { validationError: { target: false } }).then((errors) => {
+			if (errors.length > 0) {
+				res.status(400).send(errors);
+			}
+		});
+
+		await registerPayout(this.payoutRepository, payoutInput);
+
+		res.status(201).send();
 	}
 }
