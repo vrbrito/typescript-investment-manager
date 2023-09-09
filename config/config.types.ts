fa5e22a1e3efe0b023c48da8a10fe79e3379e9ee
Vitor Ramalho <vitor.ramalho@bct.ect.ufrn.br>
2023-09-09 19:42:45 -0300

    [SETUP] Configure local db

--- config/config.types.ts
+++ config/config.types.ts
@@ -2,6 +2,15 @@ export interface AppConfig {
 	port: number;
 }
 
+export interface DbConfig {
+	host: string;
+	user: string;
+	password: string;
+	db: string;
+	port: number;
+}
+
 export interface Config {
 	App: AppConfig;
+	Db: DbConfig;
 }
